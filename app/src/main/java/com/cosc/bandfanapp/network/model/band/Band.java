2a6b776f1d5f726b6cc1b974a022573a704f5896
William Moffitt <w.moffitt@wmoffitt.com>
2015-11-18 21:43:54 -0500

    Started working on pulling all bands

---
+++ app/src/main/java/com/cosc/bandfanapp/network/model/band/Band.java
@@ -0,0 +1,15 @@
+package com.cosc.bandfanapp.network.model.band;
+
+/**
+ * @author William Moffitt
+ * @version 1.0 11/18/15
+ */
+public class Band {
+
+    public int id;
+    public String name;
+    public String date_start;
+    public String date_thru;
+    public String modify_date;
+
+}
---
+++ app/src/main/java/com/cosc/bandfanapp/network/model/bandmember/CreateBandMemberRequest.java
@@ -0,0 +1,8 @@
+package com.cosc.bandfanapp.network.model.bandmember;
+
+/**
+ * @author William Moffitt
+ * @version 1.0 11/18/15
+ */
+public class CreateBandMemberRequest {
+}
